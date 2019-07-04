d2ebb90272c6eee8f5297792e058366f16dc2c87
dongliang.wang <371651702@qq.com>
2019-07-04 15:01:54 +0800

    commit config

--- config_client/src/main/java/com/xinhui/wdl/config/client/ConfigClientApplication.java
+++ config_client/src/main/java/com/xinhui/wdl/config/client/ConfigClientApplication.java
@@ -2,12 +2,17 @@ package com.xinhui.wdl.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author dongliang.wang
  * @createTime 2019/6/28
  **/
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
