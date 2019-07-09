6d92528235424136cc93d2c9d0c0fa8af61660bd
dongliang.wang <371651702@qq.com>
2019-07-09 15:53:59 +0800

    turbine

--- service_feign/src/main/java/com/xinhui/wdl/service/feign/ServiceFeignApplication.java
+++ service_feign/src/main/java/com/xinhui/wdl/service/feign/ServiceFeignApplication.java
@@ -1,9 +1,14 @@
 package com.xinhui.wdl.service.feign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * EnableDiscoveryClient注解是基于spring-cloud-commons依赖，并且在classpath中实现；
@@ -15,10 +20,20 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ServiceFeignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceFeignApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
---
+++ service_turbine/src/main/java/com/xinhui/wdl/service/turbine/ServiceTurbineApplication.java
@@ -0,0 +1,35 @@
+package com.xinhui.wdl.service.turbine;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author dongliang.wang
+ * @createTime 2019/7/3
+ **/
+@SpringBootApplication
+@EnableTurbine
+@EnableEurekaClient
+@EnableHystrixDashboard
+public class ServiceTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTurbineApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
+}
