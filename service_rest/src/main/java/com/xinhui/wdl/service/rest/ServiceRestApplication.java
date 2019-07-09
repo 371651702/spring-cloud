7dfe243f4c0cae26f4aeabcdb7af6db4fb1b353f
dongliang.wang <371651702@qq.com>
2019-07-09 14:00:37 +0800

    hystrix DashBoard

--- service_rest/src/main/java/com/xinhui/wdl/service/rest/ServiceRestApplication.java
+++ service_rest/src/main/java/com/xinhui/wdl/service/rest/ServiceRestApplication.java
@@ -1,11 +1,13 @@
 package com.xinhui.wdl.service.rest;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class ServiceRestApplication {
 
     public static void main(String[] args) {
@@ -28,5 +31,15 @@ public class ServiceRestApplication {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
--- service_rest/src/main/java/com/xinhui/wdl/service/rest/service/RestService.java
+++ service_rest/src/main/java/com/xinhui/wdl/service/rest/service/RestService.java
@@ -20,14 +20,19 @@ public class RestService {
     /**
      * Spring cloud有两种服务调用方式，一种是ribbon+restTemplate，另一种是feign
      * 本案例是ribbon+restTemplate方式实现
-     * @param name
-     * @return
+     * @param name  调用时传递过来的名字
+     * @return 远程调用接受的信息
      */
     @HystrixCommand(fallbackMethod = "hiError")
     public String getServiceName(String name){
         return restTemplate.getForObject("http://eureka-client/hi?name=" + name,String.class);
     }
 
+    /**
+     * 熔断时调用的方法
+     * @param name  调用时传递过来的名字
+     * @return  熔断时显示的内容
+     */
     public String hiError(String name){
         return "hi," + name + ",sorry,error!";
     }
